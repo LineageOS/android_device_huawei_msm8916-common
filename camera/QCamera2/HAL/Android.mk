ff7da713ccd33262f2cb9fa4e99f48bb90ceb9fe
Suman Mukherjee <sumam@codeaurora.org>
2016-06-13 16:18:37 +0530

    QCamera2:HAL1: Update native handle for every timestamp callback
    Incase framework updates camera native_handle pointer, we need
    to use cached native_handle to update for every timestamp
    callback. This change is excecuted only if media extension flag
    is enabled for the target.
    
    Issue:
    Use case introduced in video to overwrites camera native handle
    pointer.
    
    Fix:
    Change is to cache camera native handle and restore when video
    release camera buffer.
    
    Change-Id: I0bdc01b3f2b51d7bba66320cc8ef1ab019136d4e

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -18,6 +18,12 @@ LOCAL_SRC_FILES := \
         wrapper/QualcommCamera.cpp
 
 LOCAL_CFLAGS = -Wall -Werror -DDEFAULT_ZSL_MODE_ON -DDEFAULT_DENOISE_MODE_ON
+
+#use media extension
+#ifeq ($(TARGET_USES_MEDIA_EXTENSIONS), true)
+LOCAL_CFLAGS += -DUSE_MEDIA_EXTENSIONS
+#endif
+
 #Debug logs are enabled
 #LOCAL_CFLAGS += -DDISABLE_DEBUG_LOG
 
