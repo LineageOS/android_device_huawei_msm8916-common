19e44cc5714e2737266c0fa5f4f2f205d15f7ccc
dianlujitao <dianlujitao@gmail.com>
2016-09-19 12:54:04 +0800

    msm8916: Use GCC-4.8 to compile kernel
    Change-Id: Ib662281dd5d44254d7e6dbb2c0ad581336e6be04

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -119,6 +119,7 @@ BOARD_RAMDISK_OFFSET     := 0x02000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_DTBTOOL_ARGS := -2
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(PWD)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 TARGET_KERNEL_SOURCE := kernel/huawei/msm8916
 TARGET_KERNEL_CONFIG := cyanogenmod_cherry_defconfig
 
