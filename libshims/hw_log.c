85071c752ac757e223107469f1366cdb13973b6b
Rudolf Tammekivi <rtammekivi@gmail.com>
2016-12-30 01:44:04 +0200

    msm8916-common: Split shims
    This brings easy and clear to understand reference about where the proprietary
    library looks for the symbol.
    
    Kanged from kiwi.
    
    Change-Id: I0fc2002a7d4a618957563e394ca9bf71670f0fc5

--- libshims/hw_log.c
+++ libshims/hw_log.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The CyanogenMod Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-void ioprio_set(void) {
-}
-
-void ioprio_get(void) {
+int isLogEnabled(void) {
+    return 0;
 }
