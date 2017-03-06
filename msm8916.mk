d4e550279e0edc1c414b018c71cee9a03f0468cf
Rudolf Tammekivi <rtammekivi@gmail.com>
2017-03-06 18:25:16 +0200

    msm8916-common: Include NXP mifare support
    Change-Id: Idb38e3c68cbb723aea914629f1e8d8a917a022f9

--- msm8916.mk
+++ msm8916.mk
@@ -36,7 +36,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+    frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
+    frameworks/native/data/etc/com.nxp.mifare.xml:system/etc/permissions/com.nxp.mifare.xml
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
