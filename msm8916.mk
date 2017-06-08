ce33b67fef52a5d78b5c22e54377364c03f05658
Rudolf Tammekivi <rtammekivi@gmail.com>
2017-06-08 09:59:55 +0300

    msm8916-common: Set Huawei USB VID/PID pairs
    Since midi pair is not found in stock ROM, use the default value from
    Qualcomm.
    
    Change-Id: I79ecb757e2f67bf17d44bbef7c671487b4545817

--- msm8916.mk
+++ msm8916.mk
@@ -128,6 +128,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
 
+# USB ID
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    ro.usb.id.midi=90BA \
+    ro.usb.id.midi_adb=90BB \
+    ro.usb.id.mtp=1051 \
+    ro.usb.id.mtp_adb=1052 \
+    ro.usb.id.ptp=1083 \
+    ro.usb.id.ptp_adb=1054 \
+    ro.usb.id.ums=1037 \
+    ro.usb.id.ums_adb=1038 \
+    ro.usb.vid=12d1
+
 # WiFi
 PRODUCT_PACKAGES += \
     libwcnss_qmi
