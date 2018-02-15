f8805a75b337e1a876f2b043736bf1f366221c00
Suman Mukherjee <sumam@codeaurora.org>
2018-02-14 22:01:11 -0500

    QCamera2: Added LOCAL_VENDOR_MODULE to set output path of the binaries
    Replaced LOCAL_PROPRIETARY_MODULE with LOCAL_VENDOR_MODULE to set the
    output vendor path for hal binaries and libraries
    
    Change-Id: Ib04d80eabc0e17f3863e956db5f2378e426c0687

--- camera/mm-image-codec/qomx_core/Android.mk
+++ camera/mm-image-codec/qomx_core/Android.mk
@@ -23,5 +23,6 @@ LOCAL_SRC_FILES := qomx_core.c
 LOCAL_MODULE           := libqomx_core
 LOCAL_PRELINK_MODULE   := false
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_VENDOR_MODULE := true
 
 include $(BUILD_SHARED_LIBRARY)
