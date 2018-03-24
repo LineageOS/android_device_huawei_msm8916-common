fe07ae79efa9974b4584b29bbd71164a888eb600
dianlujitao <dianlujitao@lineageos.org>
2018-03-24 20:01:35 +0800

    msm8916: Clean up init extension
    Change-Id: Ic321615202eb908bd4b965b7097834d49cf96ef8

--- init/init_cherry.cpp
+++ init/init_cherry.cpp
@@ -27,18 +27,22 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <android-base/logging.h>
 #include <cstdlib>
 #include <fstream>
 #include <string>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
+#include "vendor_init.h"
 
 #include "init_msm8916.h"
+
+using android::init::property_set;
+using namespace std;
+
+const char *APP_INFO = "/proc/app_info";
 
 void property_override(char const prop[], char const value[])
 {
@@ -51,106 +55,99 @@ void property_override(char const prop[], char const value[])
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
+void set_model(const char *model) {
+    property_override("ro.build.product", model);
+    property_override("ro.product.device", model);
+    property_override("ro.product.model", model);
+    property_override("ro.vendor.product.device", model);
+}
+
 void init_target_properties()
 {
-    std::ifstream fin;
-    std::string buf;
+    ifstream fin;
+    string buf;
 
-    fin.open("/proc/app_info");
+    fin.open(APP_INFO, ios::in);
+    if (!fin) {
+        LOG(ERROR) << __func__ << ": Failed to open " << APP_INFO;
+        return;
+    }
+
     while (getline(fin, buf))
-        if (buf.find("huawei_fac_product_name") != std::string::npos)
+        if (buf.find("huawei_fac_product_name") != string::npos)
             break;
     fin.close();
 
     /* C8817D */
-    if (buf.find("C8817D") != std::string::npos) {
-        property_override("ro.product.model", "C8817D");
-        property_override("ro.product.device", "C8817D");
-        property_override("ro.build.product", "C8817D");
+    if (buf.find("C8817D") != string::npos) {
+        set_model("C8817D");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "8,1");
-        property_set("telephony.lteOnCdmaDevice", "1");
         property_override("ro.build.description", "C8817D-user 4.4.4 GRJ90 C92B381 release-keys");
         property_override("ro.build.fingerprint", "Huawei/C8817D/hwC8817D:4.4.4/HuaweiC8817D/C92B381:user/release-keys");
     }
     /* C8817E */
-    else if (buf.find("C8817E") != std::string::npos) {
-        property_override("ro.product.model", "C8817E");
-        property_override("ro.product.device", "C8817E");
-        property_override("ro.build.product", "C8817E");
+    else if (buf.find("C8817E") != string::npos) {
+        set_model("C8817E");
         property_set("ro.telephony.default_network", "8");
-        property_set("telephony.lteOnCdmaDevice", "1");
         property_override("ro.build.description", "C8817E-user 4.4.4 GRJ90 C92B380 release-keys");
         property_override("ro.build.fingerprint", "Huawei/C8817E/hwC8817E:4.4.4/HuaweiC8817E/C92B380:user/release-keys");
     }
     /* G621-TL00 */
-    else if (buf.find("G621-TL00") != std::string::npos) {
-        property_override("ro.product.model", "G621-TL00");
-        property_override("ro.product.device", "G621-TL00");
-        property_override("ro.build.product", "G621-TL00");
+    else if (buf.find("G621-TL00") != string::npos) {
+        set_model("G621-TL00");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "17,1");
         property_override("ro.build.description", "G621-TL00-user 4.4.4 GRJ90 C01B361SP01 release-keys");
         property_override("ro.build.fingerprint", "Honor/G621-TL00/hwG621-TL00:4.4.4/HonorG621-TL00/C01B361SP01:user/release-keys");
     }
     /* G620S-UL00 */
-    else if (buf.find("G620S-UL") != std::string::npos) {
-        property_override("ro.product.model", "G620S-UL00");
-        property_override("ro.product.device", "G620S-UL00");
-        property_override("ro.build.product", "G620S-UL00");
+    else if (buf.find("G620S-UL") != string::npos) {
+        set_model("G620S-UL00");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "9,1");
         property_override("ro.build.description", "G620S-UL00-user 4.4.4 GRJ90 C17B246 release-keys");
         property_override("ro.build.fingerprint", "Huawei/G620S-UL00/hwG620S-UL00:4.4.4/HuaweiG620S-UL00/C17B246:user/release-keys");
     }
     /* G620S-L01 */
-    else if (buf.find("G620S-L01") != std::string::npos) {
-        property_override("ro.product.model", "G620S-L01");
-        property_override("ro.product.device", "G620S-L01");
-        property_override("ro.build.product", "G620S-L01");
+    else if (buf.find("G620S-L01") != string::npos) {
+        set_model("G620S-L01");
         property_set("ro.telephony.default_network", "9");
         property_override("ro.build.description", "G620S-L01-user 4.4.4 GRJ90 C00B380 release-keys");
         property_override("ro.build.fingerprint", "Huawei/G620S-L01/hwG620S-L01:4.4.4/HuaweiG620S-L01/C00B380:user/release-keys");
     }
     /* G620S-L02 */
-    else if (buf.find("G620S-L02") != std::string::npos) {
-        property_override("ro.product.model", "G620S-L02");
-        property_override("ro.product.device", "G620S-L02");
-        property_override("ro.build.product", "G620S-L02");
+    else if (buf.find("G620S-L02") != string::npos) {
+        set_model("G620S-L02");
         property_set("ro.telephony.default_network", "9");
         property_override("ro.build.description", "G620S-L02-user 4.4.4 GRJ90 C00B380 release-keys");
         property_override("ro.build.fingerprint", "Huawei/G620S-L02/hwG620S-L02:4.4.4/HuaweiG620S-L02/C00B380:user/release-keys");
     }
     /* Che1-CL10 */
-    else if (buf.find("Che1-CL10") != std::string::npos) {
-        property_override("ro.product.model", "Che1-CL10");
-        property_override("ro.product.device", "Che1-CL10");
-        property_override("ro.build.product", "Che1-CL10");
+    else if (buf.find("Che1-CL10") != string::npos) {
+        set_model("Che1-CL10");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "8,1");
-        property_set("telephony.lteOnCdmaDevice", "1");
         property_override("ro.build.description", "Che1-CL10-user 4.4.4 GRJ90 C92B275 release-keys");
         property_override("ro.build.fingerprint", "Honor/Che1-CL10/Che1:4.4.4/Che1-CL10/C92B275:user/ota-rel-keys,release-keys");
     }
     /* Che1-CL20 */
-    else if (buf.find("Che1-CL20") != std::string::npos) {
-        property_override("ro.product.model", "Che1-CL20");
-        property_override("ro.product.device", "Che1-CL20");
-        property_override("ro.build.product", "Che1-CL20");
+    else if (buf.find("Che1-CL20") != string::npos) {
+        set_model("Che1-CL20");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "22,1");
-        property_set("telephony.lteOnCdmaDevice", "1");
         property_override("ro.build.description", "Che1-CL20-user 4.4.4 GRJ90 C00B275 release-keys");
         property_override("ro.build.fingerprint", "Honor/Che1-CL20/Che1:4.4.4/Che1-CL20/C00B275:user/ota-rel-keys,release-keys");
     }
     /* Che1-L04 */
-    else if (buf.find("Che1-L04") != std::string::npos) {
-        property_override("ro.product.model", "Che1-L04");
-        property_override("ro.product.device", "Che1-L04");
-        property_override("ro.build.product", "Che1-L04");
+    else if (buf.find("Che1-L04") != string::npos) {
+        set_model("Che1-L04");
         property_set("persist.radio.multisim.config", "dsds");
         property_set("ro.telephony.default_network", "9,1");
         property_override("ro.build.description", "Che1-L04-user 4.4.4 GRJ90 C900B130 release-keys");
         property_override("ro.build.fingerprint", "Honor/Che1-L04/Che1:4.4.4/Che1-L04/C900B130:user/ota-rel-keys,release-keys");
+    }
+    else {
+        LOG(ERROR) << __func__ << ": unexcepted huawei_fac_product_name!";
     }
 }
