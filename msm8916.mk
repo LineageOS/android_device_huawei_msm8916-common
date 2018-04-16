4bfb79e19846f2b45329f0445a825960ef9f6853
Demon Singur <demonsingur@gmail.com>
2018-04-16 08:04:00 +0000

    msm8916: dummy libril-wrapper
    * Based on commits:
    
    msm8998-common: add ril wrapper to handle unsupported DC-HSPAP rat
    msm8998-common: ril-wrapper: do not provide full path for dlopen
    
    Change-Id: I3c0fe0e473d10fff01ca31bd09c36689678ba43e

--- msm8916.mk
+++ msm8916.mk
@@ -130,6 +130,10 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc
 
+# RIL
+PRODUCT_PACKAGES += \
+    libril-wrapper
+
 # Sensors HAL
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
